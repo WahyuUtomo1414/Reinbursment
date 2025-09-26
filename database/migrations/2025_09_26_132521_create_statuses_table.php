2da994d4e552f210038d2497fa6d532b3c2096b7
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-26 20:26:30 +0700

    buat table status

---
+++ database/migrations/2025_09_26_132521_create_statuses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Traits\BaseModelSoftDelete;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use BaseModelSoftDelete;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('status', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('status_type_id')->constrained('status_type');
+            $table->string('name', 128);
+            $table->text('description')->nullable();
+            $this->base($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('status');
+    }
+};
