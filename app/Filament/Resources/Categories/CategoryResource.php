97e28766893679d9f4e9613f672ced96e31da5fe
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-28 14:35:04 +0700

    buat nav group category

--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -15,14 +15,19 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use UnitEnum;
 
 class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Tag;
+
+    protected static ?string $navigationLabel = 'Category';
 
     protected static ?string $recordTitleAttribute = 'Category';
+
+    protected static string | UnitEnum | null $navigationGroup = 'Data Reinbursement';
 
     public static function form(Schema $schema): Schema
     {
