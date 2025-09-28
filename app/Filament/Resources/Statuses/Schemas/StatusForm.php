9fbd77c1c72023660cc11cd03c7749261d05e6f3
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-28 13:06:08 +0700

    edit form status

--- app/Filament/Resources/Statuses/Schemas/StatusForm.php
+++ app/Filament/Resources/Statuses/Schemas/StatusForm.php
@@ -2,10 +2,11 @@
 
 namespace App\Filament\Resources\Statuses\Schemas;
 
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 
 class StatusForm
 {
@@ -13,21 +14,24 @@ class StatusForm
     {
         return $schema
             ->components([
-                TextInput::make('status_type_id')
-                    ->required()
-                    ->numeric(),
+                Select::make('status_type_id')
+                    ->relationship('statusType', 'name')
+                    ->createOptionForm([
+                        TextInput::make('name')
+                            ->columnSpanFull()
+                            ->required(),
+                        Textarea::make('description')
+                            ->columnSpanFull(),
+                        Toggle::make('active')
+                            ->required(),
+                    ])
+                    ->required(),
                 TextInput::make('name')
                     ->required(),
                 Textarea::make('description')
                     ->columnSpanFull(),
                 Toggle::make('active')
                     ->required(),
-                TextInput::make('created_by')
-                    ->numeric(),
-                TextInput::make('updated_by')
-                    ->numeric(),
-                TextInput::make('deleted_by')
-                    ->numeric(),
             ]);
     }
 }
