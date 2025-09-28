034e2c51aa3e1abe48dd0c22a600142ba1cba24d
WahyuTomo14 <wahyuxd14@gmail.com>
2025-09-28 14:20:02 +0700

    edit table employe

--- app/Filament/Resources/Employes/Tables/EmployesTable.php
+++ app/Filament/Resources/Employes/Tables/EmployesTable.php
@@ -25,20 +25,18 @@ class EmployesTable
                     ->searchable(),
                 TextColumn::make('personal_number')
                     ->searchable(),
-                TextColumn::make('id_position')
-                    ->numeric()
-                    ->sortable(),
+                TextColumn::make('position.name')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Position'),
                 IconColumn::make('active')
                     ->boolean(),
-                TextColumn::make('created_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('updated_by')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('deleted_by')
-                    ->numeric()
-                    ->sortable(),
+                TextColumn::make('createdBy.name')
+                    ->label('Created By'),
+                TextColumn::make('updatedBy.name')
+                    ->label("Updated by"),
+                TextColumn::make('deletedBy.name')
+                    ->label("Deleted by"),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
