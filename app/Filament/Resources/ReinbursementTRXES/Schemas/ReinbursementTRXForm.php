10411dbc1c33d1c6e16eaea54a5c90d7cb6424f8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-08 07:52:49 +0700

    buat layout trx form

--- app/Filament/Resources/ReinbursementTRXES/Schemas/ReinbursementTRXForm.php
+++ app/Filament/Resources/ReinbursementTRXES/Schemas/ReinbursementTRXForm.php
@@ -2,10 +2,15 @@
 
 namespace App\Filament\Resources\ReinbursementTRXES\Schemas;
 
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
+use App\Models\Status;
+use App\Models\Account;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
 
 class ReinbursementTRXForm
 {
@@ -13,29 +18,40 @@ class ReinbursementTRXForm
     {
         return $schema
             ->components([
-                TextInput::make('id_account')
+                Select::make('id_account')
+                    ->label('Account')
+                    ->options(function () {
+                        return Account::where('created_by', Auth::id())
+                            ->get()
+                            ->mapWithKeys(function ($account) {
+                                // key = id, value = "provider - name"
+                                return [$account->id => "{$account->provider} - {$account->account_name}"];
+                            })
+                            ->toArray();
+                    })
                     ->required()
-                    ->numeric(),
-                TextInput::make('id_employe')
+                    ->columnSpanFull(),
+                Hidden::make('id_employe')
+                    ->label('Employee')
                     ->required()
-                    ->numeric(),
+                    ->default(function () {
+                        return Auth::user()->id_employe;
+                    }),
                 TextInput::make('total_amount')
                     ->required()
                     ->numeric()
-                    ->default(0),
+                    ->prefix('Rp. ')
+                    ->default(0)
+                    ->columnSpanFull(),
                 Textarea::make('note')
                     ->columnSpanFull(),
                 TextInput::make('approve_by'),
                 DatePicker::make('approve_at'),
-                TextInput::make('status_id')
+                Select::make('status_id')
                     ->required()
-                    ->numeric(),
-                TextInput::make('created_by')
-                    ->numeric(),
-                TextInput::make('updated_by')
-                    ->numeric(),
-                TextInput::make('deleted_by')
-                    ->numeric(),
+                    ->label('Status')
+                    ->options(Status::all()->pluck('name', 'id'))
+                    ->columnSpanFull(),
             ]);
     }
 }
