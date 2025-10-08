c0adc269a6dd4c437207a542ead094d689f7a5a5
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-08 21:13:20 +0700

    nuat divisi table

---
+++ app/Models/Division.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\AuditedBySoftDelete;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Division extends Model
+{
+    use HasFactory, Notifiable, AuditedBySoftDelete, SoftDeletes;
+    protected $table = 'division';
+    protected $guarded = ['id'];
+
+    public function employe()
+    {
+        return $this->belongsTo(Employe::class, 'division_master');
+    }
+}
