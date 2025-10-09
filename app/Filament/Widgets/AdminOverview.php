d50c9ac0787ed862c5a464c7129f0a875e18afd8
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-09 22:27:22 +0700

    update data di stats admin

--- app/Filament/Widgets/AdminOverview.php
+++ app/Filament/Widgets/AdminOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Employe;
+use App\Models\ReinbursementTRX;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
@@ -9,23 +11,29 @@ use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
 class AdminOverview extends StatsOverviewWidget
 {
     use HasWidgetShield;
-    
+
     protected function getStats(): array
     {
+        $employe = Employe::count();
+        $reinburment = ReinbursementTRX::count();
+        $reinbursementTotal = ReinbursementTRX::sum('total_amount');
+
+        $formattedTotal = 'Rp. ' . number_format($reinbursementTotal, 0, ',', '.');
+
         return [
-            Stat::make('Employees', '24')
+            Stat::make('Employees', $employe)
                 ->description('↑ 32k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success')
                 ->chart([3, 4, 6, 8, 12, 14, 18, 20, 24]),
 
-            Stat::make('Reimbursements', '34')
+            Stat::make('Reimbursements', $reinburment)
                 ->description('↓ 7% decrease')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger')
                 ->chart([10, 9, 8, 7, 6, 5, 4, 4, 3]),
 
-            Stat::make('Total Amount', 'Rp 23.000.000')
+            Stat::make('Total Amount', $formattedTotal)
                 ->description('↑ 3% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success')
