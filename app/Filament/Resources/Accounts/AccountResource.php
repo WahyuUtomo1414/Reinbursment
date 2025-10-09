ecfe81abdc3c0087e2eee41c374c551b164ae6d0
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-09 22:21:09 +0700

    buat validasi list data account

--- app/Filament/Resources/Accounts/AccountResource.php
+++ app/Filament/Resources/Accounts/AccountResource.php
@@ -2,20 +2,21 @@
 
 namespace App\Filament\Resources\Accounts;
 
-use App\Filament\Resources\Accounts\Pages\CreateAccount;
-use App\Filament\Resources\Accounts\Pages\EditAccount;
-use App\Filament\Resources\Accounts\Pages\ListAccounts;
-use App\Filament\Resources\Accounts\Schemas\AccountForm;
-use App\Filament\Resources\Accounts\Tables\AccountsTable;
-use App\Models\Account;
+use UnitEnum;
 use BackedEnum;
-use Filament\Resources\Resource;
-use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
+use App\Models\Account;
 use Filament\Tables\Table;
+use Filament\Schemas\Schema;
+use Filament\Resources\Resource;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use UnitEnum;
+use App\Filament\Resources\Accounts\Pages\EditAccount;
+use App\Filament\Resources\Accounts\Pages\ListAccounts;
+use App\Filament\Resources\Accounts\Pages\CreateAccount;
+use App\Filament\Resources\Accounts\Schemas\AccountForm;
+use App\Filament\Resources\Accounts\Tables\AccountsTable;
 
 class AccountResource extends Resource
 {
@@ -53,6 +54,27 @@ class AccountResource extends Resource
             'create' => CreateAccount::route('/create'),
             'edit' => EditAccount::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $query = parent::getEloquentQuery()
+            ->withoutGlobalScopes([SoftDeletingScope::class]);
+
+        if (!Auth::check()) {
+            return $query; // jika tidak login, tetap kembalikan query
+        }
+
+        $user = Auth::user();
+        $roles = $user->roles->pluck('name')->toArray();
+
+        // Super Admin & Finance bisa lihat semua
+        if (in_array('super_admin', $roles) || in_array('Finance', $roles)) {
+            return $query;
+        }
+
+        // Employee biasa
+        return $query->where('created_by', $user->id);
     }
 
     public static function getRecordRouteBindingEloquentQuery(): Builder
