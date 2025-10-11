e8c853143317cda7db4649e80b842347ff2ae1cf
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-11 12:06:40 +0700

    buat faker user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Employe;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -14,31 +16,21 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+        $faker = \Faker\Factory::create('id_ID');
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        $name = $faker->name();
+        $emailBase = strtolower(str_replace(' ', '.', $name));
+
+        return [
+            'name' => $name,
+            'email' => $faker->unique()->safeEmail() ?? "{$emailBase}@gmail.com",
+            'password' => Hash::make('12345678'),
+            'id_employe' => Employe::factory(),
+            'active' => true,
+        ];
     }
 }
