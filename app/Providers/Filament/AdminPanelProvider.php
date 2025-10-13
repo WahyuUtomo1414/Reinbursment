3d597af01eee2876995148ec117c9579471684fa
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-14 06:32:15 +0700

    tambah dicover di filament panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -59,6 +59,9 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentShieldPlugin::make(),
             ])
-            ->databaseNotifications();
+            ->databaseNotifications()
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters');
     }
 }
