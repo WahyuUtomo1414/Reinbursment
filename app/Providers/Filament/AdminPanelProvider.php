78a0d606c8754e93ff2ab32faf39521b26e7080b
WahyuTomo14 <wahyuxd14@gmail.com>
2025-10-16 12:57:49 +0700

    hapus global search

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -59,6 +59,7 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentShieldPlugin::make(),
             ])
-            ->databaseNotifications();
+            ->databaseNotifications()
+            ->globalSearch(false);
     }
 }
